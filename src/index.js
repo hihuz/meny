9e86962cf230666a04e28aa251c5a9374c5c56d7
Sylvain Boulade <bopulie@gmail.com>
2017-01-16 21:26:45 +0100

    Add some basic reducer logic

--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ import { createStore } from 'redux';
 import { BrowserRouter } from 'react-router';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import throttle from 'lodash/throttle';
+import { loadState, saveState } from './utils/localStorage';
 import rootReducer from './reducers/root';
 import App from './views/App';
 
 const store = createStore(
   rootReducer,
+  loadState(),
   typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f
 );
+
+store.subscribe(throttle(() => {
+  const { token } = store.getState();
+  saveState({
+    token
+  });
+}), 1000);
 
 render(
   <BrowserRouter>
---
+++ src/reducers/recipes.js
@@ -0,0 +1,14 @@
+import sample from '../../sample.json';
+
+const DEFAULT_STATE = sample
+
+const recipes = (state = DEFAULT_STATE, action) => {
+  switch (action.type) {
+    case 'TEMP':
+      return Object.assign({}, action.recipes);
+    default:
+      return state;
+  }
+};
+
+export default recipes;
