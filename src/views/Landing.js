00d031585f90d426779fd37ee272088d1cbc70ff
Sylvain Boulade <bopulie@gmail.com>
2017-01-20 17:53:29 +0100

    Add webpack extract text plugin

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
-import NavBar from './NavBar';
+import NavBar from '../components/NavBar';
 
+// use props.children for ContentContainer ?
 const Landing = (props) => (
   <div>
     <NavBar />
