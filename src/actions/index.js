936df0023e804851e79ce6dec75b711d52f0255f
Sylvain Boulade <bopulie@gmail.com>
2017-02-21 21:02:16 +0100

    Fix unfortunate typo in actions.js :(

--- src/actions/index.js
+++ src/actions/index.js
@@ -211,7 +211,7 @@ export function addNewRecipe(recipe, user) {
         text: 'Ajouter une autre recette'
       },
       right: {
-        path: `/recipe/${newRecipeKey},
+        path: `/recipe/${newRecipeKey}`,
         text: 'Voir la page de votre recette'
       }
     };
