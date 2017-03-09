cc4a1a56f963db8e1e10fac0d7506716e51b750a
Sylvain Boulade <bopulie@gmail.com>
2017-03-09 21:40:17 +0100

    Update todo list

--- src/index.js
+++ src/index.js
@@ -39,9 +39,7 @@ TODO :
   after that implement button in InputListForm to save / undo changes
   then change all the other infos to be editable
 
-- Add.js : autofocus is set on the lowest "InputListForm" instead of top of the page, fix that
-
-- Add.js : add some sort of markdown / formatting for
+- Add.js : add some sort of markdown / formatting for the textareas
   Right now not even the line breaks are showing :c
 
 - Add.js : add transition + delay for error messages so that they don't appear instantly ?
@@ -55,27 +53,24 @@ TODO :
   This will be fixed when I add storage for the recipes so I can make a quick fix
   For example adding the recipe fetching to App.js since it is importing firebase anyway
 
+- Add a modal for confirmation on important actions (delete, edit maybe ?)
+
 // Browse.js :
 - add first to last / last to first settings for triage
 - add triage by rating (later)
 - add pagination of results ?
 
-- FIX react router keeping the scrolling intact on route change!
 - Change avatar (also in menu items) to use image instead of background image maybe ?
 - Add transition delay as inline style for user menu items
 - Convert the NavBar to be dynamic based off route
 - add "fake" loader as top bar (http://ricostacruz.com/nprogress/)
-- add a generic loader component (the same one as my twitch project ?)
 - Add a <form> html element to the Landing page header so that enter navigates to "browse"
 
 - use firebase rest api instead of sdk to shave client bytes
 
 // Add.js :
 - Fix the "e" key reseting the numeric field ?
-- Fix minor issue when the "delete" button on a input list is being clicked,
-  it triggers the error message (onBlur)
 - Add a recap of errors near the submit button ? (not necessary)
-- Fix the ingredients form getting rendered twice on load
 
 // RecipePage.js :
 - Check if it is best to not save changes right off the bat and
@@ -88,7 +83,7 @@ TODO :
 - Service Worker : check offline-plugin for webpack (check reference from kent c. dodds)
 - Try to see if I can cache the recipes for offline and only update the modified records,
   check localforage, redux persist..
-- localforage looks good
+  localforage looks good
 - find some improvements to start downloading other common routes :
   this should be automatic with offline-plugin / service worker (otherwise try prefetch plugin)
   from the landing page after it is interactive
