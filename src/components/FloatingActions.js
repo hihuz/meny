6a886747add9f5e560877366a42b5f24ddeb9286
Sylvain Boulade <bopulie@gmail.com>
2017-03-28 20:47:53 +0200

    Testing animations

--- src/components/FloatingActions.js
+++ src/components/FloatingActions.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import FloatingEditButton from './FloatingEditButton';
 import FloatingConfirmButtons from './FloatingConfirmButtons';
 
 const FloatingActions = ({ editing, switchMode, cancelChanges, saveChanges, isValid }) => (
   <div className="floating-actions">
-    {editing ?
-      <FloatingConfirmButtons
-        cancelChanges={cancelChanges}
-        saveChanges={saveChanges}
-        isValid={isValid}
-      /> :
-      <FloatingEditButton switchMode={switchMode} />
-    }
+    <ReactCSSTransitionGroup
+      transitionName="test"
+      transitionEnterTimeout={300}
+      transitionLeaveTimeout={300}
+    >
+      {editing ?
+        <FloatingConfirmButtons
+          key="fab_0"
+          cancelChanges={cancelChanges}
+          saveChanges={saveChanges}
+          isValid={isValid}
+        /> :
+        <FloatingEditButton switchMode={switchMode} key="fab_1" />
+      }
+    </ReactCSSTransitionGroup>
   </div>
 );
 
