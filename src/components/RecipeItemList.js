9f93883b38a9bd5ef5f01ea1e9e3bc0aa79c63f1
Sylvain Boulade <bopulie@gmail.com>
2018-07-22 08:26:55 +0200

    Properly handle line breaks in recipes

--- src/components/RecipeItemList.js
+++ src/components/RecipeItemList.js
@@ -1,20 +1,14 @@
-import React from 'react';
-import TextWithLineBreaks from '../components/TextWithLineBreaks';
+import React from "react";
 
-const RecipeItemList = ({
-  listItems,
-  listTitle
-}) => (
+const RecipeItemList = ({ listItems, listTitle }) => (
   <section className="section">
     <p className="section__title">{listTitle}</p>
     {listItems.map((item, i) => (
       <div className="flex-container" key={i}>
         <p className="recipe-list__index">
-          <span>
-            {i + 1}
-          </span>
+          <span>{i + 1}</span>
         </p>
-        <TextWithLineBreaks className="recipe-list__label" text={item} />
+        <p className="recipe-list__label">{item}</p>
       </div>
     ))}
   </section>
