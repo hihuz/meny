1eafa502ecf0001a73cd9bce739ace6b0567a570
Sylvain Boulade <bopulie@gmail.com>
2018-07-22 16:11:05 +0200

    Update dependencies and webpack config
    This is a pretty big change : major versions changes for react, webpack, babel, eslint, prettier was added...

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,22 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
+const autoprefixer = require("autoprefixer");
 
 module.exports = env => ({
   context: __dirname,
-  entry: "./src/index.js",
+  mode: env.prod ? "production" : "development",
+  entry: path.resolve(__dirname, "src/index.js"),
   devtool: env.prod ? "cheap-module-source-map" : "eval",
   output: {
-    path: path.join(__dirname, "/dist"),
+    path: path.join(__dirname, "dist"),
     filename: "bundle.js",
     publicPath: "/"
   },
   devServer: {
     historyApiFallback: true
-  },
-  resolve: {
-    extensions: [".js", ".json"]
   },
   stats: {
     colors: true,
@@ -33,20 +32,50 @@ module.exports = env => ({
         exclude: /node_modules/
       },
       {
-        test: /\.json$/,
-        loader: "json-loader"
-      },
-      {
         include: path.resolve(__dirname, "src"),
         test: /\.js$/,
-        loader: "babel-loader"
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                modules: false,
+                loose: true,
+                targets: {
+                  browsers: ["last 2 versions", "Explorer 11"]
+                },
+                useBuiltIns: "usage"
+              }
+            ],
+            "@babel/react"
+          ],
+          plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/syntax-dynamic-import"]
+        }
       },
       {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: ["css-loader"]
-        })
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: () => [
+                autoprefixer({
+                  browsers: [">1%", "last 4 versions", "Firefox ESR", "not ie < 9"]
+                })
+              ]
+            }
+          },
+          "sass-loader"
+        ]
       }
     ]
   },
@@ -54,7 +83,10 @@ module.exports = env => ({
     new HtmlWebpackPlugin({
       template: "./src/index.html.ejs"
     }),
-    new ExtractTextPlugin("styles.css"),
+    new MiniCssExtractPlugin({
+      filename: "[id].[contenthash:8].css",
+      chunkFilename: "[id].[contenthash:8].css"
+    }),
     new CopyWebpackPlugin([
       { from: "public", to: "public" },
       { from: "favicon.ico" },
