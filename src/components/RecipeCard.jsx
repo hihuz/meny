4e08eb58745b5ceb1da142bb1ad6139e948cad34
Sylvain Boulade <sboulade@gmail.com>
2025-06-13 13:44:35 +0200

    Fix recipe card overflow

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -5,16 +5,20 @@ const RecipeCard = ({ id, background, title, text, infos }) => (
   <RecipeLink id={id} type="card">
     <div className="card__image" style={{ backgroundImage: `url(/${background}.jpg)` }} />
     <div className="card__content">
-      <h3 className="card__title">{title}</h3>
-      <div className="card__infos">
-        <div>
-          <i className="icon-clock-o" /> {`${infos.prepTime}'`}
+      <div className="card__wrapper">
+        <div className="card__title">
+          <h3 className="card__title__content">{title}</h3>
         </div>
-        <div>
-          <img src="/pan.svg" alt="pan" /> {`${infos.cookingTime}'`}
-        </div>
-        <div>
-          <i className="icon-group" /> {infos.servings}
+        <div className="card__infos">
+          <div>
+            <i className="icon-clock-o" /> {`${infos.prepTime}'`}
+          </div>
+          <div>
+            <img src="/pan.svg" alt="pan" /> {`${infos.cookingTime}'`}
+          </div>
+          <div>
+            <i className="icon-group" /> {infos.servings}
+          </div>
         </div>
       </div>
       <div className="card__text">{text}</div>
